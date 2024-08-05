5381142dc4fa83f032aa8c9f3b9f51ff83da9952
VigneshG3123 <168328143+VigneshG3123@users.noreply.github.com>
2024-08-05 13:30:09 +0300

    Create main.py

---
+++ app/main.py
@@ -0,0 +1,34 @@
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+from .database import engine, SessionLocal
+import models
+from .schemas import ChoiceBase, QuestionBase
+
+app = FastAPI()
+
+models.Base.metadata.create_all(bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+db_dependency = Depends(get_db)
+
+@app.post("/questions/")
+async def create_questions(question: QuestionBase, db: db_dependency):
+    db_question = models.Question(question_text=question.question_text)
+    db.add(db_question)
+    db.commit()
+    db.refresh(db_question)
+    for choice in question.choices:
+        db_choice = models.Choice(
+            choice_text=choice.choice_text,
+            is_correct=choice.is_correct,
+            question_id=db_question.id
+        )
+        db.add(db_choice)
+    db.commit()
+    return db_question
